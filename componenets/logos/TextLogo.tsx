aec6d04b1b2e44a0f37c3869abf9f659b8ca4d02
mohammad maso <mohammadmasoudie@gmail.com>
2021-07-19 11:55:55 +0430

    -

--- componenets/logos/TextLogo.tsx
+++ componenets/logos/TextLogo.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Box, Text } from '@chakra-ui/react';
 import Image from 'next/image';
-import { appName } from '../../constants';
 
 export default function TextLogo(props: any) {
   return (
@@ -11,7 +10,6 @@ export default function TextLogo(props: any) {
         height={props.height ?? 100}
         width={props.width ?? 100}
         layout="intrinsic"
-        alt={appName}
       />
     </Box>
   );
--- theme/theme.js
+++ theme/theme.js
@@ -2,11 +2,40 @@ import { extendTheme } from '@chakra-ui/react';
 import { mode } from '@chakra-ui/theme-tools';
 
 const colors = {
-  primary: "#215970",
-  second: "#378695",
-
-
+  primary: {
+    100: "#009d21",
+    200: "#4BF970",
+    300: "#84FBA0",
+    400: "#009d21",
+    500: "#009d21",
+    600: "#009d21",
+    700: "#009d21",
+    800: "#009d21",
+    900: "#009d21",
+  },
 };
+
+const components = {
+    Button: { baseStyle: { _focus: { boxShadow: "none" } } },
+    Switch: {
+      baseStyle: {
+        track: {
+          _focus: {
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+    Tabs: {
+      baseStyle: {
+        tab: {
+          _focus: {
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  }
 
 const fonts = {
     heading: "iranyekan",
@@ -14,6 +43,6 @@ const fonts = {
 }
 const direction = "rtl"
 
-const theme = extendTheme({direction, colors, fonts });
+const theme = extendTheme({direction, colors, fonts , components});
 
 export default theme;
