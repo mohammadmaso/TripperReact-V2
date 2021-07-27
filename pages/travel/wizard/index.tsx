386fb3aa20f84acdf4e2309eeb2a92e414b1ae6e
mohammad maso <mohammadmasoudie@gmail.com>
2021-07-27 16:45:28 +0430

    add pages

---
+++ pages/travel/wizard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import BaseLayout from '../../../layouts/BaseLayout';
+
+export default function Home() {
+  return (
+    <BaseLayout>
+      <div></div>
+    </BaseLayout>
+  );
+}
