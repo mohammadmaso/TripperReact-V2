263ab46054a681e0b1e88fbea55ad98a60dccd46
mohammad maso <mohammadmasoudie@gmail.com>
2021-08-10 12:05:23 +0430

    fix thiss sheet

--- componenets/PWAInstallPrompt.tsx
+++ componenets/PWAInstallPrompt.tsx
@@ -8,6 +8,7 @@ import {
   Heading,
   Text,
   CloseButton,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import * as React from 'react';
 import { useAddToHomescreenPrompt } from '../hooks/useAddToHomescreenPrompt';
@@ -16,53 +17,70 @@ export function PWAInstallPrompt() {
   const [prompt, promptToInstall] = useAddToHomescreenPrompt();
   const [isVisible, setVisibleState] = React.useState(false);
 
+  const isMobile = useBreakpointValue({
+    base: true,
+    sm: true,
+    md: false,
+    lg: false,
+  });
   const hide = () => setVisibleState(false);
   const { isOpen, onToggle } = useDisclosure();
 
+  const cancle = () => {
+    localStorage.setItem('pwa-prompt-showed', '1');
+    setVisibleState(false);
+  };
+
   React.useEffect(() => {
+    const isCancled = localStorage.getItem('pwa-prompt-showed');
     if (prompt) {
-      setVisibleState(true);
+      if (isCancled != '1') {
+        setVisibleState(true);
+      }
     }
   }, [prompt]);
 
   if (!isVisible) {
     return <div />;
   }
-
-  return (
-    <>
-      {/* <Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}> */}
-      <Box
-        roundedTop="md"
-        bgColor="#009d21"
-        boxShadow="inner"
-        bottom="0"
-        zIndex="overlay"
-        position="fixed"
-        //   h="26vh"
-        w="full"
-        p="3"
-      >
-        <Stack justify="center" textAlign="center" textColor="white">
-          <CloseButton mb="-40px" onClick={hide} />
-          <Image src="/CircleLogo.svg" h="5rem" alt="Logo" onClick={hide} />
-          <Heading fontWeight="bold" fontSize="lg">
-            تجربه سریع‌تر و امکان مرور آفلاین
-          </Heading>
-          <Text fontWeight="light" fontSize="lg">
-            با نصب نسخه وب تریپر
-          </Text>
-          <Button
-            onClick={promptToInstall}
-            variant="outline"
-            fontWeight="light"
-            rounded="full"
-          >
-            نصب سریع
-          </Button>
-        </Stack>
-      </Box>
-      {/* </Slide> */}
-    </>
-  );
+  if (isMobile) {
+    return (
+      <>
+        {/* <Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}> */}
+        <Box
+          roundedTop="md"
+          bgColor="#009d21"
+          boxShadow="inner"
+          bottom="0"
+          zIndex="overlay"
+          position="fixed"
+          //   h="26vh"
+          w="full"
+          p="3"
+        >
+          <Stack justify="center" textAlign="center" textColor="white">
+            <CloseButton mb="-40px" onClick={cancle} />
+            <Image src="/CircleLogo.svg" h="5rem" alt="Logo" onClick={cancle} />
+            <Heading fontWeight="bold" fontSize="lg">
+              تجربه سریع‌تر و امکان مرور آفلاین
+            </Heading>
+            <Text fontWeight="light" fontSize="lg">
+              با نصب نسخه وب تریپر
+            </Text>
+            <Button
+              onClick={promptToInstall}
+              variant="outline"
+              fontWeight="light"
+              rounded="full"
+            >
+              نصب سریع
+            </Button>
+          </Stack>
+        </Box>
+        {/* </Slide> */}
+      </>
+    );
+  } else {
+    return null;
+  }
 }
