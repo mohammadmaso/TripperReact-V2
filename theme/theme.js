3daa2bbab3c38a7f94105cbe63272ddd18252fc7
mohammad maso <mohammadmasoudie@gmail.com>
2021-09-17 16:27:24 +0430

    update theme

--- theme/theme.js
+++ theme/theme.js
@@ -59,14 +59,14 @@ const fonts = {
 }
 const direction = "rtl"
 
-// const breakpoints = createBreakpoints({
-//   sm: "320px",
-//   md: "768px",
-//   lg: "1200px",
-//   xl: "1600px",
-// })
+const breakpoints = createBreakpoints({
+  sm: "330px",
+  md: "768px",
+  lg: "1200px",
+  xl: "1600px",
+})
 
-const theme = extendTheme({direction, colors, fonts , components, config, sizes:{
+const theme = extendTheme({direction, colors, fonts , components, breakpoints, config, sizes:{
   fontSizes:{
     md: "0.7rem",
     sm: "0.5rem"
