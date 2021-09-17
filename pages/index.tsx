8bc6da5fc23f2c823e25ac5071b92e5d4b69b508
mohammad maso <mohammadmasoudie@gmail.com>
2021-09-17 17:03:13 +0430

    add slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import TripCarousal from '../componenets/carousals/TripCarousal';
 import WikiCarusal from '../componenets/carousals/WikiCarousal';
 import CallToActionCreateGroupTrip from '../componenets/landing/CallToActionCreateGroupTrip';
 import CallToActionWithVideo from '../componenets/landing/CallToActionWithVideo';
+import LandingGallery from '../componenets/landing/Slider';
 
 import BaseLayout from '../layouts/BaseLayout';
 import LastArticlesView from '../views/article/LastArticlesView';
@@ -13,7 +14,8 @@ import TripCategoriesView from '../views/trip/TripCategoriesView';
 export default function Home() {
   return (
     <BaseLayout>
-      <CallToActionWithVideo />
+      <LandingGallery />
+      {/* <CallToActionWithVideo /> */}
       <LastTripsView />
       <TripCategoriesView />
       {/* <CallToActionCreateGroupTrip /> */}
