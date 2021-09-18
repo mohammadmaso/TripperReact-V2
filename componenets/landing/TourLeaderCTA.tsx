4aebde6a879dfddbcccda342c94d600bb703a996
mohammad maso <mohammadmasoudie@gmail.com>
2021-09-18 15:18:04 +0430

    tourLeader cta

---
+++ componenets/landing/TourLeaderCTA.tsx
@@ -0,0 +1,39 @@
+import { Button } from '@chakra-ui/button';
+import { Box, Center, Heading, Text, Wrap } from '@chakra-ui/layout';
+import Link from 'next/link';
+import React from 'react';
+
+interface Props {}
+
+const TourLeaderCTA = (props: Props) => {
+  return (
+    <Box
+      backgroundImage={
+        'url(/images/slider/nika-tchokhonelidze-Ms_p0I5DQSM-unsplash.jpg)'
+      }
+      backgroundSize={'cover'}
+      backgroundPosition={'center center'}
+      w="full"
+      h="30vh"
+      rounded="md"
+    >
+      <Center rounded="md" flexDirection="column" h="full" bgColor="#00000040">
+        <Wrap direction="column" justify="center" align="center">
+          <Text textColor="white" fontWeight="black" fontSize="3xl">
+            تور لیدر هستی؟
+          </Text>
+          <Text textColor="white" fontWeight="bold" fontSize="MD">
+            تور بعدی خودت رو اینجا حرفه‌ای بساز...
+          </Text>
+          <Link href="/auth/tourguid/register" passHref>
+            <Button rounded="sm" fontSize="sm" colorScheme="whiteAlpha">
+              ثبت نام تور لیدر‌ها
+            </Button>
+          </Link>
+        </Wrap>
+      </Center>
+    </Box>
+  );
+};
+
+export default TourLeaderCTA;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import WikiCarusal from '../componenets/carousals/WikiCarousal';
 import CallToActionCreateGroupTrip from '../componenets/landing/CallToActionCreateGroupTrip';
 import CallToActionWithVideo from '../componenets/landing/CallToActionWithVideo';
 import LandingGallery from '../componenets/landing/Slider';
+import TourLeaderCTA from '../componenets/landing/TourLeaderCTA';
 
 import BaseLayout from '../layouts/BaseLayout';
 import LastArticlesView from '../views/article/LastArticlesView';
@@ -14,7 +15,8 @@ import TripCategoriesView from '../views/trip/TripCategoriesView';
 export default function Home() {
   return (
     <BaseLayout>
-      <LandingGallery />
+      {/* <LandingGallery /> */}
+      <TourLeaderCTA />
       {/* <CallToActionWithVideo /> */}
       <LastTripsView />
       <TripCategoriesView />
