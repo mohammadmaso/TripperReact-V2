d5bd6e9290cf29233e4715f8fe7f3d5250ae1190
mohammad maso <mohammadmasoudie@gmail.com>
2021-09-28 12:09:40 +0330

    gi

--- next.config.js
+++ next.config.js
@@ -10,11 +10,11 @@ module.exports = {
 // const withPWA = require('next-pwa')
 // const runtimeCaching = require('next-pwa/cache')
 
-// module.exports = withPWA({
-//   pwa: {
-//     dest: 'public',
-//     runtimeCaching,
-//     register: true,
-//     skipWaiting: true,
-//   },
-// })
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+    register: true,
+    skipWaiting: true,
+  },
+})
