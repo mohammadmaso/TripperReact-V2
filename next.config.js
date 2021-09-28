c98dc35f343ac72a36c893448da97b953dc0670c
mohammad maso <mohammadmasoudie@gmail.com>
2021-09-28 12:10:09 +0330

    ---

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
 // module.exports = withBundleAnalyzer({})
 
 const withPWA = require('next-pwa')
-// const runtimeCaching = require('next-pwa/cache')
+const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
