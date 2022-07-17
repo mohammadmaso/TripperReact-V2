acbc3ee42d2bd24ece57f68f0a3b037f6714b1a8
mohammad maso <mohammadmasoudie@gmail.com>
2022-07-17 14:03:10 +0430

    add close button to stickybanner

--- components/sticky-banner.tsx
+++ components/sticky-banner.tsx
@@ -1,43 +1,56 @@
-import { Flex, Link, Text } from '@chakra-ui/react';
+import { Button, Flex, Icon, Link, Text } from '@chakra-ui/react';
 import siteConfig from '../site.config';
+import { AiOutlineClose } from 'react-icons/ai';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 type Props = {};
 
 const StickyBanner = (props: Props) => {
+  const [closed, setClosed] = useState(false);
   return (
-    <Flex
-      as={Link}
-      href={siteConfig.url.youtube}
-      bg={'primary.200'}
-      color="gray.900"
-      alignItems="center"
-      position="sticky"
-      top={0}
-      zIndex={999}
-      justifyContent="center"
-      py="8px"
-      _hover={{ textDecoration: 'none', bg: 'primary' }}
-      target="_blank"
-      //   onClick={() =>
-      //     event({
-      //       category: 'Subscription',
-      //       action: 'Clicked the YouTube banner',
-      //       label: 'Sticky YouTube banner on Top',
-      //     })
-      //   }
-    >
-      <Text as="span" fontWeight={'light'} fontSize="xs">
-        <Text as="span">
-          شما در حال استفاده از نسخه‌ی آزمایشی تریپر هستید.{' '}
-          <Text as="span" display={['none', 'inline']}>
-            گزارش ایرادات را از طریق ایمیل یا شبکه‌های اجتماعی تریپر با ما در{' '}
-            میان بگذارید.
+    <>
+      {!closed && (
+        <Flex
+          // as={Link}
+          // href={siteConfig.url.youtube}
+          bg={'primary.200'}
+          color="gray.900"
+          alignItems="center"
+          position="sticky"
+          top={0}
+          zIndex={999}
+          justifyContent="center"
+          py="8px"
+          _hover={{ textDecoration: 'none', bg: 'primary' }}
+          // target="_blank"
+          //   onClick={() =>
+          //     event({
+          //       category: 'Subscription',
+          //       action: 'Clicked the YouTube banner',
+          //       label: 'Sticky YouTube banner on Top',
+          //     })
+          //   }
+        >
+          <Text as="span" fontWeight={'light'} fontSize="xs">
+            <Text as="span">
+              شما در حال استفاده از نسخه‌ی آزمایشی تریپر هستید.{' '}
+              <Text as="span" display={['none', 'inline']}>
+                گزارش ایرادات را از طریق ایمیل یا شبکه‌های اجتماعی تریپر با ما
+                در میان بگذارید.
+              </Text>
+            </Text>
           </Text>
-        </Text>
-      </Text>
-    </Flex>
+          <Icon
+            position={'absolute'}
+            right="10px"
+            as={AiOutlineClose}
+            onClick={() => setClosed(true)}
+            cursor="pointer"
+          />
+        </Flex>
+      )}
+    </>
   );
 };
 
