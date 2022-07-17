cbff4c2e0117f59b1f4f04d44b1c7fcb6d5b56d8
mohammad maso <mohammadmasoudie@gmail.com>
2022-07-17 09:55:59 +0430

    fix responsive trip carousal

--- components/carousals/GroupTripCarousal.tsx
+++ components/carousals/GroupTripCarousal.tsx
@@ -21,8 +21,8 @@ export default function GroupTripCarousal(props: Props): ReactElement {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite: true,
           dots: true,
         },
