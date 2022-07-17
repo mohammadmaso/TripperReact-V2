e344777e42ae459584812788a2845f03b0933124
mohammad maso <mohammadmasoudie@gmail.com>
2022-07-17 09:49:14 +0430

    add site config

---
+++ site.config.js
@@ -0,0 +1,22 @@
+const siteConfig = {
+//   "author": "Mohammad Masoudie",
+  "title": "راهکار جامع گردشگری | تریپر",
+  "name": "mytripper.ir",
+  "description": "",
+  "twitter": "mohammadmaso",
+  "facebook": "mohammadmaso",
+  "logo": "/brand.png",
+  "logoSquare": "/brand-square.png",
+  "url": {
+    "web": "https://beta.mytripper.ir",
+    "twitter": "",
+    "youtube": "",
+  },
+  "keywords": [
+    "travel",
+    "social"
+  ]
+}
+
+
+export default siteConfig
