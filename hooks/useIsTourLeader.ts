c05da01666d2d21f5c355dae80988f1893963cb7
mohammad maso <mohammadmasoudie@gmail.com>
2022-07-18 14:41:11 +0430

    fix userType

--- hooks/useIsTourLeader.ts
+++ hooks/useIsTourLeader.ts
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 
 export const useIsTourLeader = () => {
   const { data, loading } = useMeQuery();
-  const isTourLeader = data?.me?.userType == "T" ? true : false 
+  const isTourLeader = false
+  // const isTourLeader = data?.me?.userType == "T" ? true : false 
   return isTourLeader
 };
