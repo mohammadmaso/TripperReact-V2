0b58cf0a2d0b55d14e7a1dc85f2a11ed8091f769
mohammad maso <mohammadmasoudie@gmail.com>
2022-07-21 12:57:18 +0430

    first test failed SX

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -5,9 +5,9 @@ test('should navigate to the about page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('http://localhost:3000/')
   // Find an element with the text 'About Page' and click on it
-  await page.click('text=About Page')
+  await page.click('text=درباره تریپر')
   // The new url should be "/about" (baseURL is used there)
   await expect(page).toHaveURL('http://localhost:3000/about')
   // The new page should contain an h1 with "About Page"
-  await expect(page.locator('h1')).toContainText('About Page')
+  await expect(page.locator('h1')).toContainText('در مسیر یک رویا')
 })
